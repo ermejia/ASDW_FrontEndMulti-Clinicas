2dbade7b261c748968b3a387e421225305dc466b
ermejia <mejiagalveze@outlook.com>
2020-11-15 02:06:38 -0600

    Creación del Menú con el Listado de Componentes para mostrar

--- AS2FrontEnd/src/app/app.component.ts
+++ AS2FrontEnd/src/app/app.component.ts
@@ -12,7 +12,39 @@ export class AppComponent {
   constructor(private router:Router){
   }
 
+
   List(){
     this.router.navigate(["list"]);
   }
+  ListDoctor(){
+    this.router.navigate(["listDoctors"]);
+  }
+  ListPatient(){
+    this.router.navigate(["listPatient"]);
+  }
+  ListClinic(){
+    this.router.navigate(["listClinics"]);
+  }
+  ListMedicine(){
+    this.router.navigate(["listMedicine"]);
+  }
+  ListRoom(){
+    this.router.navigate(["listRoom"]);
+  }
+  ListPatientRoom(){
+    this.router.navigate(["listPatientRoom"]);
+  }
+  ListAppointment(){
+    this.router.navigate(["listAppointments"]);
+  }
+  History(){
+    this.router.navigate(["history-patient"]);
+  }
+  Diagnostic(){
+    this.router.navigate(["addDiagnostic"]);
+  }
+  Appointment(){
+    this.router.navigate(["add-appoint"]);
+  }
+
 }
